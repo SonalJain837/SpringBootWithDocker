ac7ddc4c19476a8ffb2c6c24b129ab26c233bc82
Sonal <jainsonal837@gmail.com>
2021-10-26 17:11:27 +0530

    updating docker file

--- src/main/java/com/topic/RestfulApiSpringBoot/controller/TopicController.java
+++ src/main/java/com/topic/RestfulApiSpringBoot/controller/TopicController.java
@@ -4,10 +4,10 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,9 +34,11 @@ public class TopicController {
 		return ResponseEntity.created(location).build();
 	}
 	
+	
 	/*
-	 * @GetMapping("/gettopic") public List<Topic> getAllTopic() { return
-	 * topicService.getAllTopic(); }
+	 * @GetMapping(value = "/gettopic", produces = MediaType.APPLICATION_JSON_VALUE)
+	 * public List getAllTopic() { return topicService.getAllTopic(); }
 	 */
+	 
 	
 }
